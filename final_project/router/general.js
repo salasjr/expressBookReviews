82006d2208975408dd4b123f5ba5b1a62e9f5e7f
salasjr <110789948+salasjr@users.noreply.github.com>
2023-03-30 16:44:31 +0300

    update general.js with asynch- await function

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,6 +2,7 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+let axios = require("axios")
 const public_users = express.Router();
 
 
@@ -18,20 +19,34 @@ public_users.post("/register", (req,res) => {
     }
   } 
   return res.status(404).json({message: "Unable to register user."});
-
 });
 
+BooksPromise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve(books)
+    }, 2000)
+})
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).send(JSON.stringify(books,null,4));
+public_users.get('/', async (req, res) => {
+    let list_of_books
+    await BooksPromise.then((data)=>{
+        list_of_books = data
+    })
+    return res.status(200).send(JSON.stringify(list_of_books,null,4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async (req, res) => {
   const isbn = req.params.isbn;
-  filterd_book = books[isbn]
+  let list_of_books;
+  await BooksPromise.then((data)=>{
+      list_of_books = data
+  })
+  console.log(list_of_books)
+  filterd_book = list_of_books[isbn]
   if(filterd_book){
-    return res.status(200).send(JSON.stringify(books[isbn],null,4));
+    return res.status(200).send(JSON.stringify(filterd_book,null,4));
 
   }
   else{
@@ -41,10 +56,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async (req, res) =>{
   //Write your code here
   const author = req.params.author
-  const books_values = Object.values(books);
+  let list_of_books
+  await BooksPromise.then((data)=>{
+    list_of_books = data
+    })
+  const books_values = Object.values(list_of_books);
   const filtered_book = books_values.filter(book=> book.author === author);
   if(filtered_book.length>0){
     return res.status(200).send(JSON.stringify(filtered_book[0],null,4));
@@ -56,10 +75,14 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async (req, res) => {
   //Write your code here
   const title = req.params.title
-  const books_values = Object.values(books);
+  let list_of_books
+  await BooksPromise.then((data)=>{
+    list_of_books = data
+  })
+  const books_values = Object.values(list_of_books);
   const book_filter = books_values.filter(book=> book.title === title);
   if(book_filter.length>0){
     return res.status(200).send(JSON.stringify(book_filter[0],null,4));
